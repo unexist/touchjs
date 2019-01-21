c40247f8b086b0cf3de5e8e7f409ad10f9d5815d
unexist <none@none>
2019-01-21 10:48:55 +0100

    Updated test code

--- test.js
+++ test.js
@@ -1,25 +1,28 @@
-/* Old API */
-/*var button1 = duk_create_button("Button1");
+var l1 = new TjsLabel("Foobar");
 
-duk_bind_button(button1, function (idx) {
-    duk_print("Button " + idx);
-});
-
-var button2 = duk_create_button("Button2");
-
-duk_bind_button(button2, function (idx) {
-    duk_print("Button " + idx);
-}); */
-
-/* New API */
-var b1 = new TjsButton("Button1")
+var b1 = new TjsButton("Red")
+    .setBgColor(255, 0, 0)
     .bind(function () {
         this.print();
-    })
-    .click();
+        l1.setFgColor(255, 0, 0);
+    });
 
-var b2 = new TjsButton("Button2")
+var b2 = new TjsButton("Blue")
+    .setBgColor(0, 0, 255)
     .bind(function () {
         this.print();
-    })
-    .click();
+        l1.setFgColor.apply(l1, tjs_rgb("#0000ff"));
+    });
+
+var b3 = new TjsButton("Green")
+    .setBgColor(0, 255, 0)
+    .bind(function () {
+        tjs_quit();
+    });
+
+
+/* Dump */
+tjs_print(l1);
+tjs_print(b1);
+tjs_print(b2);
+tjs_print(b3);
