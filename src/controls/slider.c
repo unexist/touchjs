479c76b77d083c5d0c0bd8ea8200d5396b7ecdce
unexist <unexist@subforge.org>
2019-01-30 13:27:04 +0100

    Reorganized project: Moved obj-c to a single file
    --HG--
    branch : objc-separation

---
+++ src/controls/slider.c
@@ -0,0 +1,172 @@
+/**
+ * @package TouchJS
+ *
+ * @file Slider functions
+ * @copyright 2019 Christoph Kappel <unexist@subforge.org>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the GNU GPLv2.
+ * See the file COPYING for details.
+ **/
+
+#include "../touchjs.h"
+
+/**
+ * Native slider constructor
+ *
+ * @param[inout]  ctx  A #duk_context
+ **/
+
+static duk_ret_t tjs_slider_ctor(duk_context *ctx) {
+        /* Sanity check */
+    if (!duk_is_constructor_call(ctx)) {
+        return DUK_RET_TYPE_ERROR;
+    }
+
+    /* Create new userdata */
+    TjsWidget *widget = (TjsWidget *)tjs_userdata_new(ctx,
+        TJS_FLAG_TYPE_SLIDER, sizeof(TjsWidget));
+
+    if (NULL == widget) {
+        return DUK_RET_TYPE_ERROR;
+    }
+
+    /* Get arguments */
+    widget->value.asInt = duk_require_int(ctx, -1);
+    duk_pop(ctx);
+
+    tjs_super_init(ctx, (TjsUserdata *)widget);
+    tjs_touch_add((TjsUserdata *)widget);
+
+    TJS_LOG_DEBUG("flags=%d", widget->flags);
+
+    return 0;
+}
+
+/**
+ * Native slider bind prototype method
+ *
+ * @param[inout]  ctx  A #duk_context
+ **/
+
+static duk_ret_t tjs_slider_prototype_bind(duk_context *ctx) {
+    /* Sanity check */
+    duk_require_function(ctx, -1);
+
+    /* Get userdata */
+    TjsWidget *widget = (TjsWidget *)tjs_userdata_get(ctx);
+
+    if (NULL != widget) {
+        TJS_LOG_DEBUG("flags=%d", widget->flags);
+
+        /* Store slide callback */
+        duk_push_this(ctx);
+        duk_swap_top(ctx, -2);
+        duk_put_prop_string(ctx, -2, TJS_SYM_SLIDE_CB);
+        duk_pop(ctx);
+    }
+
+    /* Allow fluid.. */
+    duk_push_this(ctx);
+
+    return 1;
+}
+
+/**
+ * Native slider getPercent prototype method
+ *
+ * @param[inout]  ctx  A #duk_context
+ **/
+
+static duk_ret_t tjs_slider_prototype_getpercent(duk_context *ctx) {
+    /* Get userdata */
+    TjsWidget *widget = (TjsWidget *)tjs_userdata_get(ctx);
+
+    if (NULL != widget) {
+        TJS_LOG_DEBUG("flags=%d, percent=%d",
+            widget->flags, widget->value.asInt);
+
+        duk_push_int(ctx, widget->value.asInt);
+
+        return 1;
+    }
+
+    return 0;
+}
+
+/**
+ * Native slider setPercent prototype method
+ *
+ * @param[inout]  ctx  A #duk_context
+ **/
+
+static duk_ret_t tjs_slider_prototype_setpercent(duk_context *ctx) {
+    /* Get userdata */
+    TjsWidget *widget = (TjsWidget *)tjs_userdata_get(ctx);
+
+    if (NULL != widget) {
+        widget->flags |= TJS_FLAG_UPDATE_VALUE;
+        widget->value.asInt = duk_require_int(ctx, -1);
+
+        TJS_LOG_DEBUG("flags=%d, percent=%d",
+            widget->flags, widget->value.asInt);
+    }
+
+    /* Allow fluid.. */
+    duk_push_this(ctx);
+
+    return 1;
+}
+
+/**
+ * Native slider getValue prototype method
+ *
+ * @param[inout]  ctx  A #duk_context
+ **/
+
+static duk_ret_t tjs_slider_prototype_tostring(duk_context *ctx) {
+    /* Get userdata */
+    TjsWidget *widget = (TjsWidget *)tjs_userdata_get(ctx);
+
+    if (NULL != widget) {
+        TJS_LOG_DEBUG("flags=%d", widget->flags);
+
+        duk_push_sprintf(ctx, "flags=%d, value=%d",
+            widget->flags, widget->value.asInt);
+
+        return 1;
+    }
+
+    return 0;
+}
+
+/**
+ * Init methods for #TjsSlider
+ *
+ * @param[inout]  ctx  A #duk_context
+ **/
+
+void tjs_slider_init(duk_context *ctx) {
+    /* Register constructor */
+    duk_push_c_function(ctx, tjs_slider_ctor, 1);
+    duk_push_object(ctx);
+
+    /* Register methods */
+    duk_push_c_function(ctx, tjs_slider_prototype_bind, 1);
+    duk_put_prop_string(ctx, -2, "bind");
+
+    duk_push_c_function(ctx, tjs_slider_prototype_getpercent, 0);
+    duk_put_prop_string(ctx, -2, "getPercent");
+
+    duk_push_c_function(ctx, tjs_slider_prototype_setpercent, 1);
+    duk_put_prop_string(ctx, -2, "setPercent");
+
+    duk_push_c_function(ctx, tjs_super_prototype_setbgcolor, 3);
+    duk_put_prop_string(ctx, -2, "setBgColor");
+
+    duk_push_c_function(ctx, tjs_slider_prototype_tostring, 0);
+    duk_put_prop_string(ctx, -2, "toString");
+
+    duk_put_prop_string(ctx, -2, "prototype");
+    duk_put_global_string(ctx, "TjsSlider");
+}
--- src/touchjs.h
+++ src/touchjs.h
@@ -9,98 +9,104 @@
  * See the file COPYING for details.
  **/
 
-/* Imports and includes */
-#import <Cocoa/Cocoa.h>
+#ifndef TOUCHJS_H
+#define TOUCHJS_H 1
 
+/* Includes */
+#include <stdio.h>
 #include "duktape/duktape.h"
 
-
-
 /* Symbols */
-#define TJS_SYM_CLICK_CB "__click_cb"
-#define TJS_SYM_USERDATA "__userdata"
+#define TJS_SYM_CLICK_CB "\xff" "__click_cb"
+#define TJS_SYM_SLIDE_CB "\xff" "__slide_cb"
+#define TJS_SYM_USERDATA "\xff" "__userdata"
+
+/* Flags */
+#define TJS_FLAG_TYPE_TOUCH (1L << 0)
+#define TJS_FLAG_TYPE_COMMAND (1L << 1)
+#define TJS_FLAG_TYPE_LABEL  (1L << 2)
+#define TJS_FLAG_TYPE_BUTTON (1L << 3)
+#define TJS_FLAG_TYPE_SLIDER  (1L << 4)
+
+#define TJS_FLAG_UPDATE_COLOR_FG (1L << 10)
+#define TJS_FLAG_UPDATE_COLOR_BG (1L << 11)
+#define TJS_FLAG_UPDATE_VALUE (1L << 12)
+
+#define TJS_FLAG_READY (1L << 30)
+
+/* Combined */
+#define TJS_FLAGS_WIDGETS \
+    (TJS_FLAG_TYPE_LABEL|TJS_FLAG_TYPE_BUTTON|TJS_FLAG_TYPE_SLIDER)
+#define TJS_FLAGS_COLORS \
+    (TJS_FLAG_UPDATE_COLOR_FG|TJS_FLAG_UPDATE_COLOR_BG)
+#define TJS_FLAGS_UPDATES \
+    (TJS_FLAG_COLORS|TJS_FLAG_UPDATE_VALUE)
+
+/* Loglevel */
+#define TJS_LOGLEVEL_INFO (1L << 0)
+#define TJS_LOGLEVEL_DEBUG (1L << 1)
+#define TJS_LOGLEVEL_ERROR (1L << 2)
+#define TJS_LOGLEVEL_DUK (1L << 3)
 
 /* Macros */
 #define TJS_DSTACK(CTX) \
     tjs_dump_stack(__FUNCTION__, __LINE__, CTX);
-#define TJS_LOG(FMT, ...) \
-    tjs_log(__FUNCTION__, __LINE__, FMT, ##__VA_ARGS__);
-#define TJS_DEBUG \
-    NSLog(@"DEBUG %s:%d", __FUNCTION__, __LINE__);
+#define TJS_LOG_INFO(FMT, ...) \
+    tjs_log(TJS_LOGLEVEL_INFO, __FUNCTION__, __LINE__, FMT, ##__VA_ARGS__);
+#define TJS_LOG_DEBUG(FMT, ...) \
+    tjs_log(TJS_LOGLEVEL_DEBUG, __FUNCTION__, __LINE__, FMT, ##__VA_ARGS__);
+#define TJS_LOG_ERROR(FMT, ...) \
+    tjs_log(TJS_LOGLEVEL_ERROR, __FUNCTION__, __LINE__, FMT, ##__VA_ARGS__);
+#define TJS_LOG_DUK(FMT, ...) \
+    tjs_log(TJS_LOGLEVEL_DUK, __FUNCTION__, __LINE__, FMT, ##__VA_ARGS__);
 
-/* Flags */
-#define TJS_FLAG_TYPE_BUTTON (1L << 0)
-#define TJS_FLAG_TYPE_LABEL  (1L << 1)
-#define TJS_FLAG_TYPE_SLIDER  (1L << 2)
-
-#define TJS_FLAG_COLOR_FG (1L << 5)
-#define TJS_FLAG_COLOR_BG (1L << 6)
-
-/* Internal structs */
+/* Types */
 typedef struct tjs_color_t {
     unsigned char red;
     unsigned char green;
     unsigned char blue;
 } TjsColor;
 
-typedef struct tjs_control_t {
+typedef union tjs_value_t {
+    char *asChar;
+    int asInt;
+    double asDouble;
+} TjsValue;
+
+typedef struct tjs_userdata_t {
     int flags;
-    int idx;
-    char *title;
+} TjsUserdata;
 
-    char *callback;
+typedef struct tjs_widget_t {
+    int flags;
 
-    struct {
+    struct{
         struct tjs_color_t fg;
         struct tjs_color_t bg;
     } colors;
 
-    union {
-        char *asChar;
-        double asDouble;
-    } value;
+    union tjs_value_t value;
+} TjsWidget;
 
-    /* Obj-c */
-    NSTouchBarItemIdentifier identifier;
-    NSView *view;
-} TjsControl;
+typedef struct tjs_command_t {
+    int flags;
+
+    char *line;
+    union tjs_value_t value;
+} TjsCommand;
 
 /* Globals */
-static const NSTouchBarItemIdentifier kGroupButton = @"org.subforge.group";
-static const NSTouchBarItemIdentifier kQuit = @"org.subforge.quit";
-
-NSTouchBar *_groupTouchBar;
-NSMutableArray *_touchbarControls;
 duk_context *_ctx;
 
-/* Forward declarations */
-extern void DFRElementSetControlStripPresenceForIdentifier(NSString *, BOOL);
-extern void DFRSystemModalShowsCloseBoxWhenFrontMost(BOOL);
-
-void tjs_dump_stack(const char *func, int line, duk_context *ctx);
-void tjs_control_helper_update(TjsControl *control);
-void tjs_button_helper_click(duk_context *ctx);
-
-/* Interfaces */
-@interface NSTouchBarItem ()
-+ (void)addSystemTrayItem:(NSTouchBarItem *)item;
-@end
-
-@interface NSTouchBar ()
-/* macOS 10.14 and above */
-+ (void)presentSystemModalTouchBar:(NSTouchBar *)touchBar systemTrayItemIdentifier:(NSTouchBarItemIdentifier)identifier NS_AVAILABLE_MAC(10.14);
-
-/* macOS 10.13 and below */
-+ (void)presentSystemModalFunctionBar:(NSTouchBar *)touchBar systemTrayItemIdentifier:(NSTouchBarItemIdentifier)identifier NS_DEPRECATED_MAC(10.12.2, 10.14);
-@end
-
-@interface AppDelegate : NSObject <NSApplicationDelegate, NSTouchBarDelegate>
-@end
-
 /* touchjs.m */
-void tjs_log(const char *func, int line, const char *fmt, ...);
+void tjs_log(int loglevel, const char *func, int line, const char *fmt, ...);
 void tjs_fatal(void *userdata, const char *msg);
 void tjs_dump_stack(const char *func, int line, duk_context *ctx);
+void tjs_exit();
+
+void tjs_touch_add(TjsUserdata *userdata);
+void tjs_touch_update(TjsUserdata *userdata);
+void tjs_touch_remove(TjsUserdata *userdata);
 
 /* global.m */
 duk_ret_t tjs_global_print(duk_context *ctx);
@@ -108,31 +114,31 @@ duk_ret_t tjs_global_rgb(duk_context *ctx);
 duk_ret_t tjs_global_quit(duk_context *ctx);
 void tjs_global_init(duk_context *ctx);
 
-/* control.m */
-void tjs_control_helper_update(TjsControl *control);
-duk_ret_t tjs_control_helper_tostring(duk_context *ctx);
-duk_ret_t tjs_control_helper_setcolor(duk_context *ctx, int flag);
-duk_ret_t tjs_control_prototype_setfgcolor(duk_context *ctx);
-duk_ret_t tjs_control_prototype_setbgcolor(duk_context *ctx);
-duk_ret_t tjs_control_dtor(duk_context *ctx);
-duk_ret_t tjs_control_ctor(duk_context *ctx, int flags);
-
 /* userdata.m */
-TjsControl *tjs_control_userdata_new(duk_context *ctx, int flags, const char *title);
-TjsControl *tjs_control_userdata_get(duk_context *ctx);
+TjsUserdata *tjs_userdata_new(duk_context *ctx, int flags, size_t datasize);
+TjsUserdata *tjs_userdata_get(duk_context *ctx);
+void tjs_userdata_free(TjsUserdata *userdata);
+
+/* command.m */
+void tjs_command_init(duk_context *ctx);
+
+/* super.m */
+void tjs_super_update(TjsUserdata *userdata);
+//void tjs_super_callback_add(duk_context *ctx, const char *sym);
+void tjs_super_callback_call(duk_context *ctx, const char *sym, int nargs);
+
+duk_ret_t tjs_super_prototype_setfgcolor(duk_context *ctx);
+duk_ret_t tjs_super_prototype_setbgcolor(duk_context *ctx);
+
+void tjs_super_init(duk_context *ctx, TjsUserdata *userdata);
 
 /* button.m */
-void tjs_button_helper_click(duk_context *ctx);
-duk_ret_t tjs_button_ctor(duk_context *ctx);
-duk_ret_t tjs_button_prototype_bind(duk_context *ctx);
-duk_ret_t tjs_button_prototype_click(duk_context *ctx);
-duk_ret_t tjs_button_prototype_print(duk_context *ctx);
 void tjs_button_init(duk_context *ctx);
 
 /* label.m */
-duk_ret_t tjs_label_ctor(duk_context *ctx);
 void tjs_label_init(duk_context *ctx);
 
 /* slider.m */
-duk_ret_t tjs_slider_ctor(duk_context *ctx);
 void tjs_slider_init(duk_context *ctx);
+
+#endif /* TOUCHJS_H */
