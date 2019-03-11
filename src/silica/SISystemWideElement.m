e9d1e49898ba2534404bd9c68174721cb479154a
unexist <unexist@subforge.org>
2019-03-11 15:41:09 +0100

    Added Silica framework
    --HG--
    branch : silica

---
+++ src/silica/SISystemWideElement.m
@@ -0,0 +1,23 @@
+//
+//  SISystemWideElement.m
+//  Silica
+//
+
+#import <Cocoa/Cocoa.h>
+#import "CGSSpaces.h"
+#import "SISystemWideElement.h"
+
+@implementation SISystemWideElement
+
++ (SISystemWideElement *)systemWideElement {
+    static SISystemWideElement *sharedElement = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        AXUIElementRef elementRef = AXUIElementCreateSystemWide();
+        sharedElement = [[SISystemWideElement alloc] initWithAXElement:elementRef];
+        CFRelease(elementRef);
+    });
+    return sharedElement;
+}
+
+@end
