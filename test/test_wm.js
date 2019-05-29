152227ee895369ff4c20a741a0612abf0b6cc167
unexist <unexist@subforge.org>
2019-05-29 16:51:05 +0200

    Updated tests

--- test/test_wm.js
+++ test/test_wm.js
@@ -1,22 +1,31 @@
 /* WM */
 var wm = new TjsWM();
+
+/* Screens */
+var screens = wm.getScreens();
+
+screens.forEach(function (screen) {
+    tjs_print("screen: frame=" + screen.getFrame());
+});
+
+/* Windows */
 var wins = wm.getWindows();
 
 wins.forEach(function (win) {
-    tjs_print(
-        "win: title=" + win.getTitle() +
+    tjs_print("win: title=" + win.getTitle() +
         ", role=" + win.getRole() +
         ", subrole=" + win.getSubrole() +
+        ", frame=" + win.getFrame() +
         ", pid=" + win.getPid() +
-        ", rect=" + win.getRect() +
         ", movable=" + win.isMovable() +
         ", resizable=" + win.isResizable() +
         ", minimized=" + win.isMinimized() +
-        ", hidden=" + win.isHidden());
+        ", hidden=" + win.isHidden()
+    );
 
-    if (win.isMovable())
+    /*if (win.isMovable())
         win.setXY(10, 10);
 
     if (win.isResizable())
-        win.setWH(100, 100);
+        win.setWH(100, 100);*/
 });
