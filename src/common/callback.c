a7a8b5ec386c754c453db1eead697eafd7a0b303
unexist <unexist@subforge.org>
2019-06-18 14:34:15 +0200

    Moved userdata to common and added separate files for components

---
+++ src/common/callback.c
@@ -0,0 +1,33 @@
+/**
+ * @package TouchJS
+ *
+ * @file Callback functions
+ * @copyright 2019 Christoph Kappel <unexist@subforge.org>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the GNU GPLv2.
+ * See the file COPYING for details.
+ **/
+
+#include "callback.h"
+
+ /**
+  * Helper to call given callback
+  *
+  * @param[inout]  ctx    A #duk_context
+  * @param[in]     sym    Symbol to call if found
+  * @param[in]     nargs  Number of arguments for callback
+  **/
+
+void tjs_callback_call(duk_context *ctx, const char *sym, int nargs) {
+    duk_get_prop_string(ctx, -1 - nargs, sym); ///< Update index based on number of arguments
+
+    /* Call if callable */
+    if (duk_is_callable(ctx, -1)) {
+        duk_swap_top(ctx, -(2 + nargs)); ///< Move based on number of arguments
+        duk_pcall_method(ctx, nargs);
+        duk_pop(ctx); ///< Ignore result
+    } else {
+        duk_pop(ctx);
+    }
+}
---
+++ src/common/syms.h
@@ -0,0 +1,21 @@
+/**
+ * @package TouchJS
+ *
+ * @file Symbols header
+ * @copyright 2019 Christoph Kappel <unexist@subforge.org>
+ * @version $Id$
+ *
+ * This program can be distributed under the terms of the GNU GPLv2.
+ * See the file COPYING for details.
+ **/
+
+#ifndef TJS_SYMS_H
+#define TJS_SYMS_H 1
+
+/* Symbols */
+#define TJS_SYM_CLICK_CB "\xff" "__click_cb"
+#define TJS_SYM_SLIDE_CB "\xff" "__slide_cb"
+#define TJS_SYM_EVENT_CB "\xff" "__event_cb"
+#define TJS_SYM_USERDATA "\xff" "__userdata"
+
+#endif /* TJS_SYMS_H */
